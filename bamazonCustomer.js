43d58b6ecafebcd4bf0db82bded98f2424d773c2
msherman83 <msherman83@gmail.com>
2017-12-15 15:25:41 -0500

    able to select and subtract the correct product now.  also cleaned up the displays

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,18 +1,8 @@
-// TO DO
-//prompt 
-// ORDER
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
-
-// Check if there is enough stock.  If not display message that there isnt enough stock and restart app.
-// If there is enough stock it should subtract from the stock and display order total.
-
-
-
-
+// Required files
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// Create the SQL DB connection to Bamazon.
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -21,10 +11,11 @@ var connection = mysql.createConnection({
     user: "root",
 
     // Your password
-    password: "",
+    password: "Qwerty_123",
     database: "bamazon"
 });
 
+// Connect to the server and run the initialInventory() function.
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
@@ -33,7 +24,7 @@ connection.connect(function (err) {
 });
 
 
-
+// Display all products and current inventory.
 function initialInventory() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
@@ -44,12 +35,14 @@ function initialInventory() {
         console.log("=====================================================================\n")
         console.log("ID  |   Product Name   |   Department   |   Price   |   In Stock\n")
 
+        // Loop through the products in the DB and display them on screen.
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
         }
-        console.log("=====================================================================")
-        console.log("=====================================================================")
+        console.log("\n=====================================================================")
+        console.log("=====================================================================\n\n")
 
+        // Intialize buyPrompt() function.
         buyPrompt();
     });
 }
@@ -57,8 +50,11 @@ function initialInventory() {
 
 
 function buyPrompt() {
+    // Queries the products table of the DB.
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
+
+        // Inquirer prompts to ask what product they would like to buy and how many units of that product.
         inquirer
             .prompt([
                 {
@@ -85,22 +81,26 @@ function buyPrompt() {
                 }
             ])
             .then(function (answer) {
-                // When i select an ID and product.  Its taking the correct ID and converting it to whatever the NEXT IDs product quantity is, subtracting the number of units to buy and putting it in the place of the actual ID you selected.
 
-
-                var chosenItem = parseInt(answer.itemNum); // tried to do a - 1 here but it didnt work.
-                var product = results[chosenItem]; // What is product pulling its number from?
+                // Turn the inquirer queries and results of the DB query into variables.
+                var chosenItem = parseInt(answer.itemNum);
+                var product = results[chosenItem - 1]; // <---- Justin is my hero. Stupid "- 1" INSIDE the brackets.
                 var chosenUnits = parseInt(answer.units);
                 
-                // console.log hell
-                console.log("chosenItem - " + chosenItem + " type of " + typeof chosenItem);
-                console.log("product - " + product.stock_quantity + " type of " + typeof product.stock_quantity);
-                console.log("chosenUnits - " + chosenUnits + " type of " + typeof chosenUnits)
-                console.log(chosenUnits <= product.stock_quantity);
+                // console.log hell for testing.
+                // ====================================================================================
+                // console.log("chosenItem - " + chosenItem + " type of " + typeof chosenItem);
+                // console.log("product - " + product.stock_quantity + " type of " + typeof product.stock_quantity);
+                // console.log("chosenUnits - " + chosenUnits + " type of " + typeof chosenUnits)
+                // console.log(chosenUnits <= product.stock_quantity);
+                // ====================================================================================
 
-                // var updatedProduct = product - 1;
-
+                // Subtrack chosen units from the actual stock quantity and store into a variable.
                 var updatedStock = product.stock_quantity - chosenUnits;
+
+                // Decide if there is enough inventory in stock.
+                // If there is update stock levels and print out a receipt for the customer.
+                // If not let them know there isn't enough inventory and they need to try to order again.
                 if (chosenUnits <= product.stock_quantity) {
 
                     connection.query(
@@ -115,13 +115,19 @@ function buyPrompt() {
                         ]
                     );
 
-                    console.log("===== Receipt of Purchase =====");
-                    console.log("Quantity: " + chosenUnits + " | Total Cost: " + product.price * chosenUnits)
+                    console.log("\nThank you for your purchase!  Please come again.\n")
+                    console.log("===== Receipt of Purchase =====\n");
+                    console.log("Quantity: " + chosenUnits + " | " + "Product Name: " + product.product_name + " | " + " Total Cost: " + "$" + product.price * chosenUnits)
+                    console.log("\n===============================");
+                    console.log("===============================\n\n");
                 
+                    // Loop back to initial inventory screen.
                     initialInventory();
 
                 } else {
                     console.log("Not enough inventory!  Please order again.")
+
+                    // Loop back to initial inventory screen.
                     initialInventory();
                 } 
             });
